666eafed14ccc6ddd3fcbf284c74c8313034e38c
Sean <onewheel@gmail.com>
2016-10-10 21:14:07 +1300

    mapmanager

--- MapManager.cs
+++ MapManager.cs
@@ -1,13 +1,18 @@
 ﻿using System;
-using Sean.Shared.Comms;
+using Sean.Shared;
 
 namespace OpenTkClient
 {
     public static class MapManager
     {
-        public static void AddChunk(Message msg)
+        private static Chunks chunks = new Chunks ();
+
+        public static void AddChunk(Sean.Shared.Comms.Message msg)
         {
-            
+            var chunk = Sean.Shared.Chunk.Deserialize (msg.Data);
+            var position = msg.Map.MinPosition;
+            var coords = new ChunkCoords (ref position);
+            chunks.Add (coords, chunk);
         }
     }
 }
